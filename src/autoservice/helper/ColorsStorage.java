e48cec65f75cc7c7b82c4ca60240d427b5545f0a
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-04-27 16:00:03 +0300

    add: colors storage helper

---
+++ src/autoservice/helper/ColorsStorage.java
@@ -0,0 +1,22 @@
+package autoservice.helper;
+
+import java.awt.*;
+
+public class ColorsStorage {
+    public static Color backgroundColor    = new Color(241, 241, 241);
+    public static Color panelColor         = new Color(225, 225, 225);
+    public static Color headerColor        = new Color(45,  45,  45 );
+
+    public static Color defaultTextColor   = new Color(45,  45,  45 );
+    public static Color lightTextColor     = new Color(255, 255, 255);
+    public static Color mutedTextColor     = new Color(140, 140, 140);
+
+    public static Color buttonPrimary      = new Color(70,  130, 180);
+    public static Color buttonDanger       = new Color(178, 34,  34 );
+    public static Color buttonSuccess      = new Color(46,  139, 87 );
+    public static Color buttonNeutral      = new Color(110, 110, 110);
+
+    public static Color tableHeaderColor   = new Color(60,  90,  150);
+    public static Color tableRowSelected   = new Color(173, 216, 230);
+    public static Color tableGridColor     = new Color(210, 218, 230);
+}
