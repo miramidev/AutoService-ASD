fa5244105a1e45340ba9e422b3baac71009d15b9
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-04-27 16:01:33 +0300

    add: adding tabs that switch the scene

--- src/autoservice/ui/MainFrame.java
+++ src/autoservice/ui/MainFrame.java
@@ -1,6 +1,12 @@
 package autoservice.ui;
 
+import autoservice.helper.ColorsStorage;
+
 import javax.swing.*;
+import java.awt.*;
+
+import static autoservice.helper.ColorsStorage.backgroundColor;
+import static autoservice.helper.ColorsStorage.defaultTextColor;
 
 public class MainFrame extends JFrame {
 
@@ -10,5 +16,40 @@ public class MainFrame extends JFrame {
         setSize(1000, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(backgroundColor);
+
+        CreateTabs();
+    }
+
+    private void CreateTabs()
+    {
+        var tabs = new JTabbedPane();
+        tabs.setBackground(backgroundColor);
+        tabs.setForeground(defaultTextColor);
+
+        JPanel carsPanel = new JPanel();
+        carsPanel.setBackground(ColorsStorage.backgroundColor);
+        CreateTestTabView(carsPanel, "Автомобили");
+
+        JPanel ordersPanel = new JPanel();
+        ordersPanel.setBackground(backgroundColor);
+        CreateTestTabView(ordersPanel, "Заказы");
+
+        JPanel statsPanel = new JPanel();
+        statsPanel.setBackground(backgroundColor);
+        CreateTestTabView(statsPanel, "Статистика");
+
+        tabs.addTab("Автомобили", carsPanel);
+        tabs.addTab("Заказы", ordersPanel);
+        tabs.addTab("Статистика", statsPanel);
+        add(tabs);
+    }
+
+    private void CreateTestTabView(JPanel panel, String title)
+    {
+        JLabel label = new JLabel(title, SwingConstants.CENTER);
+        label.setFont(new Font("SansSerif", Font.BOLD, 24));
+        label.setForeground(ColorsStorage.defaultTextColor);
+        panel.add(label);
     }
 }
