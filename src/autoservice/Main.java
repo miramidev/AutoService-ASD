e77d6057c7d357692ea75c76979db2e8ed503388
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-04-27 15:28:49 +0300

    add: opening the base window

---
+++ src/autoservice/Main.java
@@ -0,0 +1,13 @@
+package autoservice;
+
+import autoservice.ui.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
---
+++ src/autoservice/ui/MainFrame.java
@@ -0,0 +1,14 @@
+package autoservice.ui;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame()
+    {
+        setTitle("Автосервис");
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
